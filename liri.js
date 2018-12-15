ede1f2b7fc2e097b95e740e8c9567f367ffa03fd
dissonance <ryanminor01u@yahoo.com>
2018-12-15 14:24:49 -0600

    setup NPMs, keys and .env

---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+var keys = require("keys.js");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
